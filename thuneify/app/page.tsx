7c4b0e52861e33b083b3a652de924f3adba86e6b
ChrisNesic <chrisnesicsup@gmail.com>
2025-03-03 18:50:29 +0100

    ajouter carrousel pour l'accueil

--- thuneify/app/page.tsx
+++ thuneify/app/page.tsx
@@ -1,12 +1,18 @@
-'use client';
+"use client";
 
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
-import { ShoppingCart, DollarSign, User, BookOpen, LogIn } from "lucide-react";
-import Link from "next/link";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 import { useAuth } from "@/context/auth-context";
+import { BookOpen, DollarSign, LogIn, ShoppingCart, User } from "lucide-react";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
 
 export default function Home() {
   const auth = useAuth();
@@ -17,47 +23,75 @@ export default function Home() {
     <div className="container mx-auto p-4">
       <Card className="mb-8">
         <CardHeader>
-          <CardTitle className="text-2xl font-bold">Bienvenue sur Thuneify</CardTitle>
+          <CardTitle className="text-2xl font-bold">
+            Bienvenue sur Thuneify {user ? `${user.firstname}` : ""}
+          </CardTitle>
         </CardHeader>
         <CardContent>
-          <p className="mb-6 text-lg">Gérez vos tâches quotidiennes et vos dépenses efficacement.</p>
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-            <Link href="/grocery">
-              <Button className="w-full flex flex-col items-center p-4 h-32">
-                <ShoppingCart className="h-8 w-8 mb-2" />
-                <span className="text-lg">Liste de courses</span>
-                <span className="text-sm text-gray-500">Gérez vos achats</span>
-              </Button>
-            </Link>
-            <Link href="/expenses">
-              <Button className="w-full flex flex-col items-center p-4 h-32">
-                <DollarSign className="h-8 w-8 mb-2" />
-                <span className="text-lg">Dépenses</span>
-                <span className="text-sm text-gray-500">Suivez vos dépenses</span>
-              </Button>
-            </Link>
-            <Link href="/account">
-              <Button className="w-full flex flex-col items-center p-4 h-32">
-                <User className="h-8 w-8 mb-2" />
-                <span className="text-lg">Compte</span>
-                <span className="text-sm text-gray-500">Gérez votre compte</span>
-              </Button>
-            </Link>
-            <Link href="/manga">
-              <Button className="w-full flex flex-col items-center p-4 h-32">
-                <BookOpen className="h-8 w-8 mb-2" />
-                <span className="text-lg">Manga</span>
-                <span className="text-sm text-gray-500">Suivez vos mangas</span>
-              </Button>
-            </Link>
-            <Link href="/login">
-              <Button className="w-full flex flex-col items-center p-4 h-32">
-                <LogIn className="h-8 w-8 mb-2" />
-                <span className="text-lg">Connexion</span>
-                <span className="text-sm text-gray-500">Connectez-vous</span>
-              </Button>
-            </Link>
-          </div>
+          <p className="mb-6 text-lg">
+            Gérez vos tâches quotidiennes et vos dépenses efficacement.
+          </p>
+          <Carousel opts={{ align: "start" }} className="w-full">
+            <CarouselContent>
+              <CarouselItem className="md:basis-1/2 lg:basis-1/3">
+                <Link href="/grocery">
+                  <Button className="w-full flex flex-col items-center p-4 h-32">
+                    <ShoppingCart className="h-8 w-8 mb-2" />
+                    <span className="text-lg">Liste de courses</span>
+                    <span className="text-sm text-gray-500">
+                      Gérez vos achats
+                    </span>
+                  </Button>
+                </Link>
+              </CarouselItem>
+              <CarouselItem className="md:basis-1/2 lg:basis-1/3">
+                <Link href="/expenses">
+                  <Button className="w-full flex flex-col items-center p-4 h-32">
+                    <DollarSign className="h-8 w-8 mb-2" />
+                    <span className="text-lg">Dépenses</span>
+                    <span className="text-sm text-gray-500">
+                      Suivez vos dépenses
+                    </span>
+                  </Button>
+                </Link>
+              </CarouselItem>
+              <CarouselItem className="md:basis-1/2 lg:basis-1/3">
+                <Link href="/account">
+                  <Button className="w-full flex flex-col items-center p-4 h-32">
+                    <User className="h-8 w-8 mb-2" />
+                    <span className="text-lg">Compte</span>
+                    <span className="text-sm text-gray-500">
+                      Gérez votre compte
+                    </span>
+                  </Button>
+                </Link>
+              </CarouselItem>
+              <CarouselItem className="md:basis-1/2 lg:basis-1/3">
+                <Link href="/manga">
+                  <Button className="w-full flex flex-col items-center p-4 h-32">
+                    <BookOpen className="h-8 w-8 mb-2" />
+                    <span className="text-lg">Manga</span>
+                    <span className="text-sm text-gray-500">
+                      Suivez vos mangas
+                    </span>
+                  </Button>
+                </Link>
+              </CarouselItem>
+              <CarouselItem className="md:basis-1/2 lg:basis-1/3">
+                <Link href="/login">
+                  <Button className="w-full flex flex-col items-center p-4 h-32">
+                    <LogIn className="h-8 w-8 mb-2" />
+                    <span className="text-lg">Connexion</span>
+                    <span className="text-sm text-gray-500">
+                      Connectez-vous
+                    </span>
+                  </Button>
+                </Link>
+              </CarouselItem>
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+          </Carousel>
         </CardContent>
       </Card>
     </div>
